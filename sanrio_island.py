bba3258300abc0576963be48ba20704eb3accd60
gez0007 <gerda.riemann07@gmail.com>
2025-04-08 14:53:43 +0300

    Update sanrio_island.py

--- sanrio_island.py
+++ sanrio_island.py
@@ -7,23 +7,64 @@ pygame.init()
 screenX = 1500
 screenY = 800
 screen = pygame.display.set_mode([screenX, screenY])
+running=True
 pygame.display.set_caption("Sanrio Island")
 clock = pygame.time.Clock()
 
-#taustapilt
-taustapilt=pygame.image.load("pilt.png")
-pygame.mixer.init()
+#Tegelane Kitty
+kitty_pilt=pygame.image.load('kitty.png')
+kitty_pilt=pygame.transform.scale(kitty_pilt,(100,100))
+kitty_liigub=pygame.image.load('kitty_walk.png')
+kitty_liigub=pygame.transform.scale(kitty_liigub, (100,100))
 
-gameover=False
-aeg=pygame.time.get_ticks()
-while not gameover:
-    # mängu sulgemine ristist
+#Tegelane Pochacco
+pochacco_pilt=pygame.image.load('pochacco.png')
+pochacco_pilt=pygame.transform.scale(pochacco_pilt, (100,100))
+pochacco_liigub=pygame.image.load('pochacco_walk.png')
+pochacco_liigub=pygame.transform.scale(pochacco_liigub, (100,100))
+
+#Tegelane Kuromi
+kuromi_pilt=pygame.image.load('kuromi.png')
+kuromi_pilt=pygame.transform.scale(kuromi_pilt, (100,100))
+kuromi_liigub=pygame.image.load('kuromi_walk.png')
+kuromi_liigub=pygame.transform.scale(kuromi_liigub, (100,100))
+
+#Alguskoordinadid
+x=200
+y=600
+samm=5
+
+pygame.key.set_repeat(1,10)
+while running:
+    # taustapilt
+    '''taustapilt=pygame.image.load("pilt.png")
+    pygame.mixer.init()'''
+
+
     for i in pygame.event.get():
+        screen.fill([255, 255, 255])
+
+        # Kuvan tegelase taustale
+        screen.blit(kitty_pilt, (x, y))
         if i.type == pygame.QUIT:
-            gameover=True
+            running = False
+            # Kui sündmuseks on klahvi allavajutamine…
+        elif i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_UP: #Kitty ei oska alla tagasi tulla :(
+                screen.fill([255, 255, 255])
+                y = y - samm
+                screen.blit(kitty_liigub, (x, y))
+            elif i.key == pygame.K_DOWN:
+                screen.fill([255, 255, 255])
+                screen.blit(kitty_liigub, (x, y))
+            #Järgnevas peame tausta panema liikuma, mitte tegelase,
+            # aga Grete pole pilti veel teinud mulle mida liigutada >:(
+            '''elif i.key == pygame.K_LEFT:
+                x = x - samm
+            elif i.key == pygame.K_RIGHT:
+                x = x + samm'''
 
-
-
-clock.tick(60)
+        clock.tick(60)
+        pygame.display.flip()
 
 pygame.quit()
