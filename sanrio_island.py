2aacad8aa5ed2fb657cd71c09e2610c29b9a7bd8
ingridroos <ingrid.roos@opilased.nrg.edu.ee>
2025-04-22 15:04:16 +0300

    Update sanrio_island.py

--- sanrio_island.py
+++ sanrio_island.py
@@ -4,26 +4,28 @@ import pygame, random
 pygame.init()
 
 # ekraani seaded
-screenX = 1500
-screenY = 800
+screenX = 1200
+screenY = 750
 screen = pygame.display.set_mode([screenX, screenY])
 running=True
 pygame.display.set_caption("Sanrio Island")
 clock = pygame.time.Clock()
 
 #Tegelane Kitty
-kitty_pilt=pygame.image.load('kitty.png')
-kitty_pilt=pygame.transform.scale(kitty_pilt,(100,100))
-kitty_liigub=pygame.image.load('kitty_walk.png')
-kitty_liigub=pygame.transform.scale(kitty_liigub, (100,100))
+kitty_pilt_flip=pygame.image.load('kitty.png')
+kitty_pilt_flip=pygame.transform.scale(kitty_pilt_flip,(100,100))
+kitty_pilt=pygame.transform.flip(kitty_pilt_flip,True,False)
+kitty_liigub_flip=pygame.image.load('kitty_walk.png')
+kitty_liigub_flip=pygame.transform.scale(kitty_liigub_flip, (100,100))
+kitty_liigub=pygame.transform.flip(kitty_liigub_flip,True,False)
 
-#Tegelane Pochacco
+#Tegelane Pochacco - flip puudu
 pochacco_pilt=pygame.image.load('pochacco.png')
 pochacco_pilt=pygame.transform.scale(pochacco_pilt, (100,100))
 pochacco_liigub=pygame.image.load('pochacco_walk.png')
 pochacco_liigub=pygame.transform.scale(pochacco_liigub, (100,100))
 
-#Tegelane Kuromi
+#Tegelane Kuromi - flip puudu
 kuromi_pilt=pygame.image.load('kuromi.png')
 kuromi_pilt=pygame.transform.scale(kuromi_pilt, (100,100))
 kuromi_liigub=pygame.image.load('kuromi_walk.png')
@@ -37,11 +39,11 @@ samm=5
 pygame.key.set_repeat(1,10)
 while running:
     # taustapilt
-    '''taustapilt=pygame.image.load("pilt.png")
-    pygame.mixer.init()'''
+    taustapilt=pygame.image.load("background.png")
+
 
     for i in pygame.event.get():
-        screen.fill([255, 255, 255])
+        screen.blit(taustapilt,(0, 0))
 
         # Kuvan Kitty taustale (tuleks lõpuks teha nii, et tegelast saab valida)
         screen.blit(kitty_pilt, (x, y))
@@ -52,11 +54,11 @@ while running:
         # Kui sündmuseks on klahvi allavajutamine…
         elif i.type == pygame.KEYDOWN:
             if i.key == pygame.K_UP: #Kitty ei oska alla tagasi tulla :(
-                screen.fill([255, 255, 255])
+                screen.blit(taustapilt,(0, 0))
                 y = y - samm
                 screen.blit(kitty_liigub, (x, y))
             elif i.key == pygame.K_DOWN:
-                screen.fill([255, 255, 255])
+                screen.blit(taustapilt,(0, 0))
                 screen.blit(kitty_liigub, (x, y))
             #Järgnevas peame tausta panema liikuma, mitte tegelase,
             # aga Grete pole pilti veel teinud mulle mida liigutada >:(
